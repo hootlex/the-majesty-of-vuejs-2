4e8b5751b637437d9608e67a08b28b0a4a4c0083
pongos <mimousteileis@gmail.com>
2017-06-09 01:53:25 +0300

    add vue file

---
+++ codes/chapter19/19.2/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div class="col-md-offset-2 col-md-8">
+      <h2>Sign In</h2>
+      <form class="form-inline">
+        <div class="form-group">
+          <input class="form-control" placeholder="Email">
+        </div>
+        <div class="form-group">
+          <input type="password" class="form-control" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-default">Sign in</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    console.log('login')
+  }
+}
+</script>
